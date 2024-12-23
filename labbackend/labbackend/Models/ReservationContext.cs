7eb2dfe68353c51e091ff45ce818313cef4a2ab3
olsamuhaxhiri <om64917@ubt-uni.net>
2024-12-23 17:10:39 +0100

    reservation
    reserved

---
+++ labbackend/labbackend/Models/ReservationContext.cs
@@ -0,0 +1,15 @@
+﻿using labbackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendLab.Models
+{
+    public class ReservationContext : DbContext
+    {
+        public ReservationContext(DbContextOptions<ReservationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+
+}
