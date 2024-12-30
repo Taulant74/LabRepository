d124685b791436f5e65fb70ae739e9d89492b5a3
rinaqerkezi <rq67748@ubt-uni.net>
2024-12-30 13:33:28 +0100

    Feedback backend
    feedback crud

---
+++ labbackend/labbackend/Models/Feedback.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace labbackend.Models
+{
+
+    [Table("Feedback")]
+    public class Feedback
+    {
+        public int FeedbackID { get; set; }
+        public int GuestID { get; set; }
+        public string FeedbackType { get; set; }
+        public string Message { get; set; }
+        public DateTime FeedbackDate { get; set; }
+    }
+
+
+}
---
+++ labbackend/labbackend/Models/FeedbackContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace labbackend.Models
+{
+    public class FeedbackContext : DbContext
+    {
+        public FeedbackContext(DbContextOptions<FeedbackContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Feedback> Feedbacks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Feedback>().ToTable("Feedback"); // Map to the correct table name
+        }
+    }
+    }
+
