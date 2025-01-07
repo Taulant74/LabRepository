9d26c543f140b09e822c98e2a4fad0952c20064d
rinaqerkezi <rq67748@ubt-uni.net>
2025-01-07 22:05:06 +0100

    Feedback Page
    feedback page

--- labfrontend/src/App.js
+++ labfrontend/src/App.js
@@ -7,16 +7,18 @@ import MainPage from './pages/MainPage';
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
 import Rooms from './pages/Rooms';
+import Feedback from './pages/Feedback';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={< Rooms />} />
+        <Route path="/" element={< Feedback/>} />
         
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/rooms" element={<Rooms />} />
       </Routes>
     </Router>
   );
