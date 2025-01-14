b9bf487af136fab97f027776eefb92ef5e52dce2
olsamuhaxhiri <om64917@ubt-uni.net>
2025-01-15 00:14:46 +0100

    reservation
    reserve

--- labfrontend/src/App.js
+++ labfrontend/src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,26 +13,25 @@ import Rooms from './pages/Rooms';
 import Feedback from './pages/Feedback';
 import Review from './pages/Review';
 import Profile from './pages/Profile';
+import Reservation from './pages/Reservation'; // Import Reservation page
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={< MainPage />} />
-        <Route path="/rooms" element={< Rooms />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/rooms" element={<Rooms />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/review" element={<Review />} />
         <Route path="/feedback" element={<Feedback />} />
-        <Route path="/event" element={<EventBooking />} /> 
+        <Route path="/event" element={<EventBooking />} />
         <Route path="/profile" element={<Profile />} />
-
-        
+        <Route path="/reservation" element={<Reservation />} /> {/* Add Reservation route */}
       </Routes>
     </Router>
   );
 };
-
 
 export default App;
