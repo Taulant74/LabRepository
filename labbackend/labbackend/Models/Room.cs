bc027adb2bba96bf57dffd75871092a9fdebf724
Taulant74 <taulantr2004@gmail.com>
2025-01-27 00:53:31 +0100

    do ndryshime n room edhe roomcontext

--- labbackend/labbackend/Models/Room.cs
+++ labbackend/labbackend/Models/Room.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace labbackend.Models
 {
+
+    [Table("Room")]
     public class Room
     {
         public int RoomID { get; set; }
         public int HotelID { get; set; }
-        public string RoomNumber { get; set; }
+        public int RoomNumber { get; set; }
         public int RoomTypeID { get; set; }
     }
 }
